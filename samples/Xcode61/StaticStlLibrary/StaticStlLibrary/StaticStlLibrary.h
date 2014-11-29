9f318de2714d4276051f1cfcad34dcf2d794a9d9
Max Vilimpoc <github@vilimpoc.org>
2014-11-29 14:07:37 +0100

    Added Static Library with STL support to Xcode Workspace.

---
+++ samples/Xcode61/StaticStlLibrary/StaticStlLibrary/StaticStlLibrary.h
@@ -0,0 +1,23 @@
+/*
+ *  StaticStlLibrary.h
+ *  StaticStlLibrary
+ *
+ *  Created by Max Vilimpoc (LUA) on 11/29/14.
+ *  Copyright (c) 2014 Max Vilimpoc. All rights reserved.
+ *
+ */
+
+#ifndef StaticStlLibrary_
+#define StaticStlLibrary_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class StaticStlLibrary
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ samples/Xcode61/StaticStlLibrary/StaticStlLibrary/StaticStlLibraryPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  StaticStlLibraryPriv.h
+ *  StaticStlLibrary
+ *
+ *  Created by Max Vilimpoc (LUA) on 11/29/14.
+ *  Copyright (c) 2014 Max Vilimpoc. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class StaticStlLibraryPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
