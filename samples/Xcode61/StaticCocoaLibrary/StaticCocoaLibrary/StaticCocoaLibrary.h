a0a81bea66203286379bae3ec9be6c8b67432e0a
Max Vilimpoc <github@vilimpoc.org>
2014-11-29 14:12:11 +0100

    Added Static Library with Cocoa support to Workspace.

---
+++ samples/Xcode61/StaticCocoaLibrary/StaticCocoaLibrary/StaticCocoaLibrary.h
@@ -0,0 +1,13 @@
+//
+//  StaticCocoaLibrary.h
+//  StaticCocoaLibrary
+//
+//  Created by Max Vilimpoc (LUA) on 11/29/14.
+//  Copyright (c) 2014 Max Vilimpoc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticCocoaLibrary : NSObject
+
+@end
---
+++ samples/Xcode61/StaticCocoaLibrary/StaticCocoaLibrary/StaticCocoaLibrary.m
@@ -0,0 +1,13 @@
+//
+//  StaticCocoaLibrary.m
+//  StaticCocoaLibrary
+//
+//  Created by Max Vilimpoc (LUA) on 11/29/14.
+//  Copyright (c) 2014 Max Vilimpoc. All rights reserved.
+//
+
+#import "StaticCocoaLibrary.h"
+
+@implementation StaticCocoaLibrary
+
+@end
