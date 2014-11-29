e6af47e3e2e042faac64a2c4c891e4c2acb41c4f
Max Vilimpoc <github@vilimpoc.org>
2014-11-29 14:23:33 +0100

    Add Command Line Executable (C++) project to Xcode samples.

---
+++ samples/Xcode61/CommandLineExecutableCpp/CommandLineExecutableCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CommandLineExecutableCpp
+//
+//  Created by Max Vilimpoc (LUA) on 11/29/14.
+//  Copyright (c) 2014 Max Vilimpoc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
