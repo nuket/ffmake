8842c5f7108005d9cf0faa74c952302e6e2977fd
Max Vilimpoc <github@vilimpoc.org>
2014-11-29 14:17:12 +0100

    Add Command Line Executable (C) project to Xcode samples.

---
+++ samples/Xcode61/CommandLineExecutableC/CommandLineExecutableC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CommandLineExecutableC
+//
+//  Created by Max Vilimpoc (LUA) on 11/29/14.
+//  Copyright (c) 2014 Max Vilimpoc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
